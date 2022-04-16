f52a1aed9a526cb0eb51ad59c4329a96681baacf
Gleb Ambrazhevich <gambrazhevich@apple.com>
2022-04-16 23:54:31 +0200

    Created `ErrorPage` with 404 and 403 cases

---
+++ src/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,34 @@
+import { Result, Button } from 'antd';
+import { useLocation } from 'react-router-dom';
+
+function Error() {
+  const { pathname } = useLocation();
+  const type = pathname === '/forbidden' ? 'forbidden' : 'notfound';
+  const config = {
+    forbidden: {
+      status: 403,
+      title: '403',
+      subTitle: 'Sorry, you are not authorized to access this page.',
+    },
+    notfound: {
+      status: 404,
+      title: '404',
+      subTitle: 'Sorry, the page you visited does not exist.',
+    },
+  }[type];
+
+  return (
+    <Result
+      status={config.status}
+      title={config.title}
+      subTitle={config.subTitle}
+      extra={
+        <Button href="/" type="primary">
+          Go to Main Page
+        </Button>
+      }
+    />
+  );
+}
+
+export default Error;
